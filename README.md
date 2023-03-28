7ecfe25a184b47605b75b25fd726e0378c18c23f
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-03-28 12:06:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Short Links Web App
+
+Built with CI4 and TailwindCSS 🔥
+
+Feel free join (Pull Request or make Issue) to this repository 🤗
+
---
+++ app/Database/Migrations/2023-03-28-044311_TBLINKS.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use function PHPSTORM_META\type;
+
+class TBLINKS extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'url_ori' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ],
+            'url_short' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME'
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('TB_LINKS');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('TB_LINKS');
+    }
+}
