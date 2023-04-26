1f4e5cc58f84243ffdb407658908614089ccf79d
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-04-26 20:36:36 +0700

    add meta desc

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Shorten Links by Infinity Programming">
     <link rel="stylesheet" href="/public/styles/output.css">
     <title>Shorten URL</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
