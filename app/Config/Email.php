0d4a8b64aa10ec8208d01758bd2221016a143e56
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-05-20 13:27:42 +0700

    move value password  to .env

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -5,7 +5,8 @@ namespace Config;
 use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
-{
+{   
+
     public string $fromEmail  = '';
     public string $fromName   = '';
     public string $recipients = '';
